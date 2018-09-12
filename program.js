a6dbb680429283da4a73d4dd328bd41814f1f6f0
Mateusz Rumiński <matt.ruminski@protonmail.com>
2018-09-12 18:18:38 +0200

    Add getOSinfo()

--- program.js
+++ program.js
@@ -1,3 +1,5 @@
+var os = require('os');
+
 process.stdin.setEncoding('utf-8');
 
 process.stdin.on('readable', function() {
@@ -16,8 +18,36 @@ process.stdin.on('readable', function() {
       console.log('Used Node.js version:', nodeVersion);
       process.exit();
       break;
+    case '/getOSinfo':
+      getOSinfo();
+      break;
     default:
       process.stderr.write('Wrong command');
       process.exit();  
     }
 });
+
+function getOSinfo() {
+  var type = os.type();
+  switch (type) {
+    case 'Darwin':
+    type = 'OSX';
+    break;
+    case 'Windows_NT':
+    type = 'Windows';
+    break;
+    default:
+    type = type;
+  }
+  var release = os.release;
+  var cpu = os.cpus()[0].model;
+  var uptime = os.uptime();
+  var user = os.userInfo();
+
+  console.log('OS:',type);
+  console.log('Release:',release);
+  console.log('CPU Model:',cpu);
+  console.log('Uptime: ~',(uptime / 60).toFixed(0), 'min');
+  console.log('User name:', user.username);
+  console.log('Home dir:', user.homedir);
+}
